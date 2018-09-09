1a46a7e57958e9c7d20d5f60363cf35465179e47
wakisaka daisuke <reds.daisuke@gmail.com>
2018-09-09 13:49:53 +0900

    Update app03.js

--- test/app03.js
+++ test/app03.js
@@ -1,5 +1,7 @@
 
-var createError = require('http-');
+var createError = require('http-errors');
+var createError1 = require('http-errors');
+var createError2 = require('http-errors');
 
 var express = require('express');
 var path = require('path');
